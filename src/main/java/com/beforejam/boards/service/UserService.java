70e26b347b6ecb22beb9a3f4f920986613ee28cb
Beforejamn1 <beforejamn1@gmail.com>
2026-06-01 14:25:28 +0900

    feat(service): 회원가입 중복 검증 로직 구현 (GREEN)
    - UserService.signUp() Fail-Fast 기반 중복 체크 로직 추가
    
    - UserServiceTest 전체 통과

--- src/main/java/com/beforejam/boards/service/UserService.java
+++ src/main/java/com/beforejam/boards/service/UserService.java
@@ -5,12 +5,20 @@ import com.beforejam.boards.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserMapper userMapper;
 
     public void signUp(User user){
+        User foundUser = userMapper.findByUsername(user.getUsername());
 
+        if(Objects.nonNull(foundUser)){
+            throw new IllegalArgumentException("이미 존재하는 아이디입니다.");
+        }
+
+        userMapper.save(user);
     }
 }
